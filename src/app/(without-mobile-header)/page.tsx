a24a7a510cd918aef0d1ff06c4f3a9b9b08761e2
NapolDeveloper <loma1016@gmail.com>
2025-06-02 15:25:31 +0900

    feat: 메인 배너 영역 렌더링 구현

--- src/app/(without-mobile-header)/page.tsx
+++ src/app/(without-mobile-header)/page.tsx
@@ -1,6 +1,7 @@
 import { Text } from '@/components/ui/text';
 import { getPopularCampaigns } from '@/service/campaign/campaign-api';
 
+import MainBanner from './_components/main-banner';
 import PopularSection from './_components/popular-section';
 
 export const revalidate = 10;
@@ -15,6 +16,10 @@ export default async function Home() {
 
   return (
     <>
+      <section className="md:px-6 md:py-10 lg:px-16">
+        <MainBanner />
+      </section>
+
       <section className="px-6 py-10 lg:px-16">
         <Text as="h2" size="2xl" weight="bold" className="mb-4">
           인기 캠페인
