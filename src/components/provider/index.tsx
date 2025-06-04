60f4aaab699526f88c7e8a6d8faf051502cbfb79
NapolDeveloper <loma1016@gmail.com>
2025-06-04 23:54:34 +0900

    feat: 임시 다크모드 제거

--- src/components/provider/index.tsx
+++ src/components/provider/index.tsx
@@ -1,12 +1,12 @@
 import QueryProvider from './query-provider';
 import SonnerProvider from './sonner-provider';
-import ThemeProvider from './theme-provider';
+// import ThemeProvider from './theme-provider';
 
 export default function Provider({ children }: { children: React.ReactNode }) {
   return (
-    <ThemeProvider>
+    <>
       <SonnerProvider />
       <QueryProvider>{children}</QueryProvider>
-    </ThemeProvider>
+    </>
   );
 }
