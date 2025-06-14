7855f43c986b2a01606122f732792c1c7ae32bc0
NapolDeveloper <loma1016@gmail.com>
2025-06-14 16:40:59 +0900

    feat: 캠페인 카드 스켈레톤 수정

--- src/app/(with-mobile-header)/mypage/applications/_components/campaign-item-skeleton.tsx
+++ src/app/(with-mobile-header)/mypage/applications/_components/campaign-item-skeleton.tsx
@@ -10,13 +10,20 @@ export default function CampaignItemSkeleton() {
   return (
     <Card className="flex-row gap-2 border-none py-0 shadow-none">
       {/* 이미지 스켈레톤 */}
-      <CardContent className="h-[115px] w-[115px] p-0 md:h-[105px] md:w-[105px] lg:h-[135px] lg:w-[135px]">
+      <CardContent className="relative h-[115px] w-[115px] overflow-hidden rounded-lg p-0 md:h-[105px] md:w-[105px] lg:h-[135px] lg:w-[135px]">
         <Skeleton className="h-full w-full rounded-lg" />
       </CardContent>
 
       {/* 텍스트 스켈레톤 */}
-      <CardFooter className="flex-1 flex-col items-start justify-start px-4">
-        <Skeleton className="h-4 w-3/4" />
+      <CardFooter className="flex-1 cursor-pointer flex-col items-start justify-start gap-2 px-4">
+        {/* 캠페인 타입 배지 스켈레톤 */}
+        <Skeleton className="h-5 w-16 rounded-full" />
+
+        {/* 제목과 상품 정보 스켈레톤 */}
+        <div className="flex w-full flex-col gap-1">
+          <Skeleton className="h-4 w-3/4" />
+          <Skeleton className="h-3 w-1/2" />
+        </div>
       </CardFooter>
     </Card>
   );
