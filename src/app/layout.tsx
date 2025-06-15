b74ea01725d41278335a72e6c645415c5a406323
NapolDeveloper <loma1016@gmail.com>
2025-06-15 14:58:48 +0900

    fix: 테스트를 위해 챗봇 비활성화

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,5 @@
 import localFont from 'next/font/local';
-import Script from 'next/script';
+// import Script from 'next/script';
 
 import type { Metadata } from 'next';
 
@@ -43,10 +43,8 @@ export default function RootLayout({
       >
         <Provider>{children}</Provider>
       </body>
-      <Script src="https://cdn.botpress.cloud/webchat/v3.0/inject.js" defer />
-      <Script src="https://files.bpcontent.cloud/2025/06/12/16/20250612165435-J4VLJ03G.js" defer />
+      {/* <Script src="https://cdn.botpress.cloud/webchat/v3.0/inject.js" defer />
+      <Script src="https://files.bpcontent.cloud/2025/06/12/16/20250612165435-J4VLJ03G.js" defer /> */}
     </html>
   );
 }
-
-
