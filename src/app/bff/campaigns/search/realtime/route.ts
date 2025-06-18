e4b6a58787c05758556ba1e95b0ebb48b69b4dd1
NapolDeveloper <loma1016@gmail.com>
2025-06-18 21:49:55 +0900

    feat: 매일 자정 1회 캐시 무효화 처리

--- src/app/bff/campaigns/search/realtime/route.ts
+++ src/app/bff/campaigns/search/realtime/route.ts
@@ -1,7 +1,22 @@
+import { revalidateTag } from 'next/cache';
 import { NextResponse } from 'next/server';
+
+let lastResetDate: string | null = null;
 
 // 실시간 인기 검색어 조회
 export async function GET() {
+  // 1. 지금 요청이 들어온 시간이 오후 12시가 지났는지 확인
+  const now = new Date();
+  const kstNow = new Date(now.getTime() + 9 * 60 * 60 * 1000);
+  const currentHour = kstNow.getHours();
+  const today = kstNow.toDateString();
+
+  // 2. 오후 12시가 지났고 오늘 아직 캐시 무효화 안했다면 실행
+  if (currentHour >= 12 && lastResetDate !== today) {
+    revalidateTag('search-realtime');
+    lastResetDate = today;
+  }
+
   const response = await fetch(
     `${process.env.NEXT_PUBLIC_KOK_BASE_URL}/campaigns/search/realtime?limit=5`,
     {
@@ -11,7 +26,7 @@ export async function GET() {
       },
       next: {
         // 30분 마다 초기화
-        revalidate: 1000 * 60 * 30,
+        revalidate: 60 * 30,
         tags: ['search-realtime'],
       },
     },
