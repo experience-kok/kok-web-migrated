b81f460c51de28146ef7a109d7e2cd1281f78332
NapolDeveloper <loma1016@gmail.com>
2025-08-02 02:30:36 +0900

    feat: 캠페인 지원 버튼 스켈레톤 디자인 수정

--- src/app/mobile/campaign/[campaignId]/@applicate/_components/campaign-applicate-skeleton.tsx
+++ src/app/mobile/campaign/[campaignId]/@applicate/_components/campaign-applicate-skeleton.tsx
@@ -6,10 +6,10 @@ import { Skeleton } from '@/components/ui/skeleton';
 export default function CampaignApplicateSkeleton() {
   return (
     <div>
-      <div className="fixed right-0 bottom-0 left-0 z-50 border-t border-gray-200 bg-white shadow-lg">
-        <div className="p-4">
+      <div className="fixed right-0 bottom-0 left-0 z-50 bg-white shadow-lg">
+        <div className="p-4 pt-0">
           <div className="flex items-center gap-2">
-            <Skeleton className="h-11 w-full rounded-md" />
+            <Skeleton className="h-[53px] w-full rounded-[12px]" />
           </div>
         </div>
       </div>
