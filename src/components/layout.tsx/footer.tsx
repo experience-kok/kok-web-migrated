710f6b964e070e1b3fcb9bb6076fe0fcd7a202d8
NapolDeveloper <loma1016@gmail.com>
2025-09-16 23:32:12 +0900

    feat: 푸터 수정

--- src/components/layout.tsx/footer.tsx
+++ src/components/layout.tsx/footer.tsx
@@ -13,16 +13,19 @@ export default function Footer() {
           체험콕
         </Text>
         <div className="text-muted-foreground mt-2 text-xs">
-          회사명 | 대표이사: 이순신 | 개인정보 보호 최고책임자: 삼순신 <br />
-          사업자등록번호: 123-11-12345 | 통신판매업신고번호: 제0000-서울강남-00000호 <br />
-          주소: 서울특별시 OO구 OO로 OOO길 OO (논현동) <br />
+          {/* 회사명 | 대표이사: 이순신 | 개인정보 보호 최고책임자: 삼순신 <br /> */}
+          {/* 사업자등록번호: 123-11-12345 | 통신판매업신고번호: 제0000-서울강남-00000호 <br /> */}
+          {/* 주소: 서울특별시 OO구 OO로 OOO길 OO (논현동) <br /> */}
           메일:{' '}
-          <a href="mailto:help@revu.net" className="text-muted-foreground hover:underline">
-            help@kok.net
+          <a
+            href="mailto:chkok.official@gmail.com"
+            className="text-muted-foreground hover:underline"
+          >
+            chkok.official@gmail.com
           </a>{' '}
-          | 전화: 1234-1234 | 팩스: 02-123-4567 <br />
+          {/* | 전화: 1234-1234 | 팩스: 02-123-4567 <br /> */}
           <Text size="sm" as="p" color="foreground" className="mt-4" weight="bold">
-            &copy; {currentYear} Wings of Innovation. All rights reserved.
+            &copy; {currentYear} KOK. All rights reserved.
           </Text>
         </div>
       </div>
