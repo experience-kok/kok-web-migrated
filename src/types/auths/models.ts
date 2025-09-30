b28f2936b1b775e3c800dd53abdbb8bcc166e23a
NapolDeveloper <loma1016@gmail.com>
2025-09-30 12:30:44 +0900

    feat: API 코드 리팩토링

---
+++ src/types/auths/models.ts
@@ -0,0 +1,30 @@
+// 유저 권한
+export const ROLE = {
+  USER: '유저',
+  CLIENT: '기업고객',
+  ADMIN: '관리자',
+} as const;
+export type Role = keyof typeof ROLE;
+
+// 유저 성별
+export const GENDER = {
+  MALE: '남',
+  FEMALE: '여',
+  UNKNOWN: '알수없음',
+} as const;
+export type Gender = keyof typeof GENDER;
+
+// 유저 타입
+export interface User {
+  id: number;
+  email: string | null;
+  nickname: string;
+  profileImage: string | null;
+  phone: string | null;
+  gender: Gender;
+  age: number | null;
+  role: Role;
+}
+
+// 로그인 타입
+export type LoginType = 'registration' | 'login';
---
+++ src/types/auths/responses.ts
@@ -0,0 +1,35 @@
+import { LoginType, User } from './models';
+
+/**
+ * 신규 유저 로그인 응답
+ * 회원가입시 신규 유저일 경우 동의 항목을 받아야합니다.
+ */
+export interface NewUserLoginResponse {
+  loginType: 'consentRequired';
+  tempToken: string;
+  user: {
+    id: number;
+    email: string;
+    nickname: string;
+    profileImageUrl: string;
+  };
+}
+
+/**
+ * 기존 유저 로그인 응답
+ * 기존에 가입된 유저일 경우 동의 항목을 받지 않고 메인 페이지로 이동합니다.
+ */
+export interface ExistingUserLoginResponse {
+  user: User;
+  accessToken: string;
+  refreshToken: string;
+  loginType: LoginType;
+}
+
+/**
+ * 유저 로그인 응답
+ * 유저 로그인은 2가지로 분리되며 아래 유니온 타입에 포함됩니다.
+ *
+ * /api/auth/login, api/auth/kakao API를 이용해 로그인시 아래 타입을 반환합니다.
+ */
+export type LoginResponse = ExistingUserLoginResponse | NewUserLoginResponse;
